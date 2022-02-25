59ee542715e3f788128a0eb123442eee17cb627c
MegaMech <MegaMech@users.noreply.github.com>
2022-02-24 18:44:53 -0700

    Update Packets.ts

--- mp/src/mk64mp/Packets.ts
+++ mp/src/mk64mp/Packets.ts
@@ -3,12 +3,14 @@ import {
   } from 'modloader64_api/ModLoaderDefaultImpls';
 import {mk64Player} from "./MK64Core";
 
+
+
 export class mk64mp_PlayerPacket extends Packet {
-    players: mk64Player;
+    localPlayer: mk64Player;
     
-    constructor(lobby: string, players: mk64Player) {
-        super('Z64O_ScenePacket', 'Z64Online', lobby, true);
-        this.players = mk64Player;
+    constructor(lobby: string, localPlayer: mk64Player) {
+        super('mk64mp_PlayerPacket', 'mk64mp', lobby, true);
+        this.localPlayer = localPlayer;
     }
 }
 
