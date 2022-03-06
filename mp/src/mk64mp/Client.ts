5d62a4152aa0afb3e9dfed743043cf315dfd8b06
MegaMech <MegaMech@users.noreply.github.com>
2022-03-06 00:25:33 -0800

    Add files via upload

--- mp/src/mk64mp/Client.ts
+++ mp/src/mk64mp/Client.ts
@@ -43,7 +43,7 @@ export class mk64mpClient {
         setTimeout(() => {
             this.pointerTable = this.helperFunc.getPointerTable(this.pointerTable)
             this.ModLoader.logger.info("RUNNING");
-            this.ModLoader.logger.info("table: "+this.pointerTable.toString())
+            this.ModLoader.logger.info(this.helperFunc.defines.gPlayer1.toString(16));
             if (this.pointerTable.length === 0) {return;}
             this.beginRead = true;
             
@@ -51,31 +51,31 @@ export class mk64mpClient {
     }
     @onTick()
     onTick(frame: number): void {
-        if (this.beginRead) {
-            this.localPlayer.posX = this.ModLoader.emulator.rdramRead32(this.pointerTable[0] + 0x14);
-            this.localPlayer.posY = this.ModLoader.emulator.rdramRead32(this.pointerTable[0] + 0x18);
-            this.localPlayer.posZ = this.ModLoader.emulator.rdramRead32(this.pointerTable[0] + 0x1C);
-            this.localPlayer.rotX = this.ModLoader.emulator.rdramRead32(this.pointerTable[0] + 0x20);
-            this.localPlayer.rotY = this.ModLoader.emulator.rdramRead32(this.pointerTable[0] + 0x24);
-            this.localPlayer.rotZ = this.ModLoader.emulator.rdramRead32(this.pointerTable[0] + 0x28);
-            this.localPlayer.turn = this.ModLoader.emulator.rdramReadS16(this.pointerTable[0] + 0x2E);
-            this.ModLoader.clientSide.sendPacket(
-                new mk64mp_PlayerPacket(
-                    this.ModLoader.clientLobby, 
-                    this.localPlayer,   
-                )
-            );
-        }
-        if (this.ModLoader.emulator.rdramRead32(0x800DC510) == 3 && !this.alreadySet) {
+         if (this.beginRead) {
+             this.localPlayer.posX = this.helperFunc.read(this.helperFunc.defines.gPlayer1 + 0x14, 32);
+             this.localPlayer.posY = this.helperFunc.read(this.helperFunc.defines.gPlayer1 + 0x18, 32);
+             this.localPlayer.posZ = this.helperFunc.read(this.helperFunc.defines.gPlayer1 + 0x1C, 32);
+             this.localPlayer.rotX = this.helperFunc.read(this.helperFunc.defines.gPlayer1 + 0x20, 32);
+             this.localPlayer.rotY = this.helperFunc.read(this.helperFunc.defines.gPlayer1 + 0x24, 32);
+             this.localPlayer.rotZ = this.helperFunc.read(this.helperFunc.defines.gPlayer1 + 0x28, 32);
+             this.localPlayer.turn = this.helperFunc.read(this.helperFunc.defines.gPlayer1 + 0x2E, 32);
+             this.ModLoader.clientSide.sendPacket(
+                 new mk64mp_PlayerPacket(
+                     this.ModLoader.clientLobby, 
+                     this.localPlayer,   
+                 )
+             );
+         }
+        if (this.helperFunc.read(this.helperFunc.defines.D_800DC510, 16) == 3 && !this.alreadySet) {
             this.alreadySet = true;
             // 800DC510
             setTimeout(() => {
                 this.setPlayersHuman();
             }, 20);
         }
-        if (this.ModLoader.emulator.rdramRead32(0x800E86A0) == 0xD && !this.alreadySet2) {
+        if (this.helperFunc.read(this.helperFunc.defines.gMenuSelection, 32) == 0xD && !this.alreadySet2) {
             this.alreadySet2 = true;
-            let myCharacter = this.ModLoader.emulator.rdramRead8(0x8018EDE4);
+            let myCharacter = this.helperFunc.read(this.helperFunc.defines.gCharacterGridSelections, 8);
             this.ModLoader.clientSide.sendPacket(
                 new packet_SelectedCharacter (
                     this.ModLoader.clientLobby, 
@@ -88,6 +88,7 @@ export class mk64mpClient {
 
     @onViUpdate()
     onViUpdate() {
+        
         this.ModLoader.ImGui.begin("Client "+this.players[this.ModLoader.me.uuid].index.toString()+" Debug", this.isDebugWindowOpen); {
             //let client = this.players[this.ModLoader.me.uuid];
             Object.entries(this.players).forEach((e, t) => {
@@ -106,7 +107,7 @@ export class mk64mpClient {
             if (this.ModLoader.me.uuid !== e[0]) {
                 let offset = (this.players[e[0]].index + 1) * this.playerStructOffset;
                 this.ModLoader.logger.info("OFFSET: "+(offset+this.pointerTable[0]).toString(16));
-                this.ModLoader.emulator.rdramWrite16(this.pointerTable[0]+offset, 0xC010);
+                this.ModLoader.emulator.rdramWrite16(this.helperFunc.defines.gPlayer1+offset, 0xC010);
             } else {
                 this.ModLoader.logger.info("Client: MY INDEX IS: "+this.players[e[0]].index.toString());
             }
@@ -156,7 +157,7 @@ export class mk64mpClient {
     @NetworkHandler(mk64Events.ON_SELECTED_CHARACTER)
     OnSelectedCharacter(packet: packet_SelectedCharacter) {
         this.players[packet.player.uuid].characterTableIndex = packet.character;
-        this.ModLoader.emulator.rdramWrite8(0x8018EDE4+this.players[packet.player.uuid].index + 1, packet.character);
+        this.ModLoader.emulator.rdramWrite8(this.helperFunc.defines.gCharacterGridSelections+this.players[packet.player.uuid].index + 1, packet.character);
         
         //this.players
         //0x0254
@@ -169,13 +170,13 @@ export class mk64mpClient {
             return;
         }
         offset = (this.players[packet.player.uuid].index + 1) * this.playerStructOffset;
-        this.ModLoader.emulator.rdramWrite32(this.pointerTable[0]+offset+0x14, packet.localPlayer.posX);
-        this.ModLoader.emulator.rdramWrite32(this.pointerTable[0]+offset+0x18, packet.localPlayer.posY);
-        this.ModLoader.emulator.rdramWrite32(this.pointerTable[0]+offset+0x1C, packet.localPlayer.posZ);
-        this.ModLoader.emulator.rdramWrite32(this.pointerTable[0]+offset+0x20, packet.localPlayer.rotX);
-        this.ModLoader.emulator.rdramWrite32(this.pointerTable[0]+offset+0x24, packet.localPlayer.rotY);
-        this.ModLoader.emulator.rdramWrite32(this.pointerTable[0]+offset+0x28, packet.localPlayer.rotZ);
-        this.ModLoader.emulator.rdramWrite16(this.pointerTable[0]+offset+0x2E, packet.localPlayer.turn);
+        this.ModLoader.emulator.rdramWrite32(this.helperFunc.defines.gPlayer1+offset+0x14, packet.localPlayer.posX);
+        this.ModLoader.emulator.rdramWrite32(this.helperFunc.defines.gPlayer1+offset+0x18, packet.localPlayer.posY);
+        this.ModLoader.emulator.rdramWrite32(this.helperFunc.defines.gPlayer1+offset+0x1C, packet.localPlayer.posZ);
+        this.ModLoader.emulator.rdramWrite32(this.helperFunc.defines.gPlayer1+offset+0x20, packet.localPlayer.rotX);
+        this.ModLoader.emulator.rdramWrite32(this.helperFunc.defines.gPlayer1+offset+0x24, packet.localPlayer.rotY);
+        this.ModLoader.emulator.rdramWrite32(this.helperFunc.defines.gPlayer1+offset+0x28, packet.localPlayer.rotZ);
+        this.ModLoader.emulator.rdramWrite16(this.helperFunc.defines.gPlayer1+offset+0x2E, packet.localPlayer.turn);
         //this.ModLoader.logger.info(packet.localPlayer.posX.toString(16)+
         //                        " | "+packet.localPlayer.posY.toString(16)+" | "+
         //                        packet.localPlayer.posZ.toString(16));
