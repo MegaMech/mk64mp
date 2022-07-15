c91a554e5ae29f514c553147a352a3cd5eaa78eb
MegaMech <MegaMech@users.noreply.github.com>
2022-07-15 01:04:14 -0600

    Create network.h

---
+++ mod/network.h
@@ -0,0 +1,10 @@
+#include <ultra64.h>
+#include <defines.h>
+#include <common_structs.h>
+#include "kart_attributes.h"
+#include "main.h"
+
+
+extern f32 *ag0;
+extern s16 netPlayerPositions[];
+extern s16 netCharacterSelections[];
